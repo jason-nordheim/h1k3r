816b7db028ca04a29eb042098f6ebb9a42734b8b
jason-nordheim <jason.nordheim@gmail.com>
2020-07-01 17:57:50 -0600

    changed the color of the navigation icons to a green

--- src/global.js
+++ src/global.js
@@ -4,11 +4,33 @@ import GroupIcon from "@material-ui/icons/Group";
 import EventIcon from "@material-ui/icons/Event";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import { green } from "@material-ui/core/colors";
 
-export const links = [
-         { address: "/", label: "Home", icon: <HomeIcon /> },
-         { address: "/social", label: "Social", icon: <GroupIcon /> },
-         { address: "/events", label: "Events", icon: <EventIcon /> },
-         { address: "/map", label: "Map", icon: <LocationOnIcon /> },
-         { address: "/map", label: "More", icon: <MoreHorizIcon /> },
+
+export const NavigationLinks = [
+         {
+           address: "/",
+           label: "Home",
+           icon: <HomeIcon style={{ color: green[700]}} />,
+         },
+         {
+           address: "/social",
+           label: "Social",
+           icon: <GroupIcon style={{ color: green[700] }} />,
+         },
+         {
+           address: "/events",
+           label: "Events",
+           icon: <EventIcon style={{ color: green[700]}} />,
+         },
+         {
+           address: "/map",
+           label: "Map",
+           icon: <LocationOnIcon style={{ color: green[700] }} />,
+         },
+         {
+           address: "/more",
+           label: "More",
+           icon: <MoreHorizIcon style={{ color: green[700]}} />,
+         },
        ];
