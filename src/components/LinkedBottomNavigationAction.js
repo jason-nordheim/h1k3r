82539161ae6780958f82aa4c44e0a65fe233d9ae
jason-nordheim <jason.nordheim@gmail.com>
2020-07-01 18:00:32 -0600

    dynamically created the link toadddress

--- src/components/LinkedBottomNavigationAction.js
+++ src/components/LinkedBottomNavigationAction.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { BottomNavigationAction } from "@material-ui/core/";
 
+
+
 export const LinkedBottomNavigationAction = ({ address, label, icon }) => {
   const location = useLocation() 
   return (
-    <Link to={address}>
+    <Link to={address} >
       <BottomNavigationAction label={label} icon={icon} />
     </Link>
   );
