f97678b7d33a2fd6a1cbb14e70f438e42d33ec89
jason-nordheim <jason.nordheim@gmail.com>
2020-07-02 13:47:17 -0600

    updated header to display the current page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { NavigationLinks } from '../global'
+import { Typography } from '@material-ui/core'
+import SettingsIcon from "@material-ui/icons/Settings";
 
-const pathMapper = {
-  '/account': 'My Account', 
-  '/social': 'Social', 
-  '/events': 'Events', 
-  
+
+const styles = {
+  header: {
+    position: 'absolute', 
+    top: 0, 
+    width: '100%',
+    display: 'flex', 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    backgroundColor: '#FFF', 
+    padding: '5px 10px'
+  }, 
+  title: {
+    flex: 3
+  }, 
+  icon: {
+    flex: 1 
+  }
 }
 
 export const MobileHeader = props => {
+  const [pageTitle, setPageTitle] = useState('Home')
   const location = useLocation() 
+  
+  useEffect(() => {
+    NavigationLinks.forEach(navLink => {
+      if(navLink.address === location.pathname) {
+        setPageTitle(navLink.label)
+      }
+    })
+  },[location])
+
   return (
-    <div>
-    </div>
-  )
+    <header style={styles.header}>
+        <Typography variant="h3"
+                  style={styles.title}
+        >
+          {pageTitle}
+        </Typography>
+        <SettingsIcon style={styles.icon} />
+    </header>
+  );
 }
