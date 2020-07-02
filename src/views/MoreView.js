8b8bb6003d0f89dc6093c81c374024641e015362
jason-nordheim <jason.nordheim@gmail.com>
2020-07-01 18:00:16 -0600

    created more view

---
+++ src/views/MoreView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MobileLayout } from '../layouts/DefaultLayout'
+
+export const MoreView = props => {
+  return (
+    <MobileLayout>
+      <h1>More</h1>
+    </MobileLayout>
+  )
+}
