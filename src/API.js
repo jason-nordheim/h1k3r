2c4a4f1865d99c84cc7a6633c337038b96ff4b74
jason-nordheim <jason.nordheim@gmail.com>
2020-07-03 15:43:05 -0600

    created post and get generic methods that can execute fetch requests to the API

--- src/API.js
+++ src/API.js
@@ -1,13 +1,39 @@
 const API_URL = "http://localhost:4000"
 
-const post = async (path, payload, authentication) => {
+const post = async (path, payload) => {
   return await fetch(API_URL + path, {
     method: "POST", 
-    headers: {
-      'Content-Type': 'application/json'
-    }, 
+    headers: { 'Content-Type': 'application/json' }, 
     body: JSON.stringify(payload)
   })
 }
+const authPost = async (path, payload, token) => {
+  return await fetch(API_URL + path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authentication: `bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
+  });
+}
+const get = async (path) => {
+  return await fetch(API_URL + path, {
+    method: "GET",
+    headers: { method: "GET", "Content-Type": "application/json" },
+  });
+}
+const authGet = async (path, token) => {
+  await fetch(API_URL + path, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authentication: `bearer ${token}`,
+    }
+  });
+}
 
-export const SignInUser = (username, password) => {};
+
+export const SignInUser = (username, password) => {
+  
+};
