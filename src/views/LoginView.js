26a0cf816a155a80ef9e04a3e1ddc5414a2636df
jason-nordheim <jason.nordheim@gmail.com>
2020-07-03 15:13:12 -0600

    added sign in form to the MobileLoginView

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MobileLayout } from '../layouts/DefaultLayout'
+import { SignInForm } from '../components/SignInForm'
 
 export const MobileLoginView = props => {
   return (
     <MobileLayout>
-      <h1>Login</h1>
+      <SignInForm /> 
     </MobileLayout>
   )
 }
