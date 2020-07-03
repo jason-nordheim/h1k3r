f04237f2d658400ca252e0517d76922ad304a38e
jason-nordheim <jason.nordheim@gmail.com>
2020-07-03 15:08:32 -0600

    changed the header  to conditionally display if the user is logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import { NavigationLinks } from '../global'
+import React, { useContext } from 'react'
 import { Typography } from '@material-ui/core'
 import SettingsIcon from "@material-ui/icons/Settings";
-
+import { usePageTitle } from '../hooks/usePageTitle';
+import { AuthorizationContext } from '../context/Authentication';
+import { Link } from 'react-router-dom';
 
 const styles = {
   header: {
-    position: 'absolute', 
-    top: 0, 
-    width: '100%',
-    display: 'flex', 
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    backgroundColor: '#FFF', 
-    padding: '5px 10px'
-  }, 
+    position: "absolute",
+    top: 0,
+    left: '-2%', 
+    width: "102%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#dedede",
+    padding: '10px 20px',
+    margin: 0, 
+    boxShadow: "5px 10px 8px #888888",
+  },
   title: {
-    flex: 3
-  }, 
+    flex: 3,
+    fontSize: "30px",
+  },
   icon: {
-    flex: 1 
-  }
-}
+    flex: 1,
+    fontSize: "32px",
+  },
+};
 
 export const MobileHeader = props => {
-  const [pageTitle, setPageTitle] = useState('Home')
-  const location = useLocation() 
-  
-  useEffect(() => {
-    NavigationLinks.forEach(navLink => {
-      if(navLink.address === location.pathname) {
-        setPageTitle(navLink.label)
-      }
-    })
-  },[location])
+  const [ pageTitle ] = usePageTitle() 
+  const AuthState = useContext(AuthorizationContext)
 
   return (
     <header style={styles.header}>
-        <Typography variant="h3"
-                  style={styles.title}
-        >
+        <Typography variant="subtitle2" style={styles.title}>
           {pageTitle}
         </Typography>
-        <SettingsIcon style={styles.icon} />
+        { AuthState.isLoggedIn 
+          ? (
+            <Link to='/account'>
+              <SettingsIcon style={styles.icon} />
+            </Link>
+          )
+          : null 
+        }
+        
     </header>
   );
 }
