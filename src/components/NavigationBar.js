c5425b20f75ee405b8c83b29a3da02cbc767bd26
jason-nordheim <jason.nordheim@gmail.com>
2020-07-03 15:09:20 -0600

    styled the navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,11 +3,22 @@ import { BottomNavigation } from '@material-ui/core/'
 import { LinkedBottomNavigationAction } from './LinkedBottomNavigationAction';
 import { NavigationLinks } from '../global'
 
+
 export const MobileNavigationBar = props => {
   return (
-    <nav>
-      <BottomNavigation style={{position: 'absolute', bottom: '0px', margin: 0, padding: 0, marginLeft: '-1vw'}}>
-        { NavigationLinks.map(link => <LinkedBottomNavigationAction key={link.label} {...link} /> ) }
+    <nav
+      style={{
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <BottomNavigation
+        style={{ backgroundColor: "#dedede", paddingTop: "5px" }}
+      >
+        {NavigationLinks.map((link) => (
+          <LinkedBottomNavigationAction key={link.label} {...link} />
+        ))}
       </BottomNavigation>
     </nav>
   );
