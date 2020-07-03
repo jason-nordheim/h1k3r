ae3792f7308400871ec513eeedbec151fbb7a5ff
jason-nordheim <jason.nordheim@gmail.com>
2020-07-03 15:10:02 -0600

    created usePageTitle custom hook

---
+++ src/hooks/usePageTitle.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+import { SiteLinks } from "../global";
+
+
+export const usePageTitle = () => {
+  const location = useLocation() 
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    SiteLinks.forEach((navLink) => {
+      if (navLink.address === location.pathname) {
+        setPageTitle(navLink.label);
+      }
+    });
+  }, [location]);
+
+  return [pageTitle]
+}
