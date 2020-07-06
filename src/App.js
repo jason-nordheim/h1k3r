9b6d42ed76e8a8864cf92e5bd2da11f7b929d8e7
jason-nordheim <jason.nordheim@gmail.com>
2020-07-06 13:48:21 -0600

    moved to inline routing vs nested

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { MobileHomeView } from "./views/HomeView";
 import { MobileLoginView } from "./views/LoginView";
@@ -8,53 +8,64 @@ import { MobileMapView } from "./views/MapView";
 import { MobileEventsView } from "./views/EventsView";
 import { MobileSocialView } from "./views/SocialView";
 import { MoreView } from "./views/MoreView";
-import { AnimatedSwitch } from "react-router-transition";
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { AuthenticationProvider } from './components/AuthenticationContext'
-
+import { AnimatedSwitch, AnimatedRoute, spring } from "react-router-transition";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthenticationProvider } from "./components/AuthenticationContext";
 
 export const App = () => {
-
+  const mapStyles = (styles) => {
+    return {
+      opacity: styles.opacity,
+      transform: `scale(${styles.scale})`,
+    };
+  };
+  const bounce = (val) => {
+    return spring(val, {
+      stiffness: 330,
+      damping: 22,
+    });
+  };
+  const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+      opacity: 0,
+      scale: 1.2,
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+      opacity: bounce(0),
+      scale: bounce(0.8),
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+      opacity: bounce(1),
+      scale: bounce(1),
+    },
+  };
 
   return (
     <BrowserRouter>
       <AuthenticationProvider>
         <AnimatedSwitch
-          atEnter={{ opacity: 0 }}
-          atLeave={{ opacity: 0 }}
-          atActive={{ opacity: 1 }}
+          atEnter={bounceTransition.atEnter}
+          atLeave={bounceTransition.atLeave}
+          atActive={bounceTransition.atActive}
+          mapStyles={mapStyles}
         >
-          <Route exact path="/">
-            <MobileHomeView />
-          </Route>
-          <Route path="/home">
-            <MobileHomeView />
-          </Route>
-          <Route path="/login">
-            <MobileLoginView />
-          </Route>
-          <Route path="/register">
-            <MobileRegisterView />
-          </Route>
-          <ProtectedRoute path="/map">
-            <MobileMapView />
-          </ProtectedRoute>
-          <ProtectedRoute path="/account">
-            <MobileAccountView />
-          </ProtectedRoute>
-          <ProtectedRoute path="/events">
-            <MobileEventsView />
-          </ProtectedRoute>
-          <ProtectedRoute path="/social">
-            <MobileSocialView />
-          </ProtectedRoute>
-          <ProtectedRoute path="/more">
-            <MoreView />
-          </ProtectedRoute>
+          <Route exact path="/" component={MobileHomeView} />
+          <Route path="/home" component={MobileHomeView} />
+          <Route path="/login" component={MobileLoginView} />
+          <Route path="/register" component={MobileRegisterView} />
+          <ProtectedRoute path="/map" component={MobileMapView} />
+          <ProtectedRoute path="/account" component={MobileAccountView} />
+          <ProtectedRoute path="/events" component={MobileEventsView} />
+          <ProtectedRoute path="/social" component={MobileSocialView} />
+          <ProtectedRoute path="/more" component={MoreView} />
+          
         </AnimatedSwitch>
       </AuthenticationProvider>
     </BrowserRouter>
   );
-}
+};
 
-export default App 
+export default App;
